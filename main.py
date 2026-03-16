e30008717bcbeaf4f23f67722214392c74eb0d07
Alexussybaka <alex10.svoboda@gmail.com>
2026-03-16 14:54:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,18 +4,35 @@ import time
 def write_stats():
     with open("stats", "r") as f:
         stats = f.read()
-        print(f"Your best score is: {stats} points.")
+        print(f"Your best score is: {get_stats()[0]} points. \nYour last score is: {get_stats()[1]} points.")
+
+def get_stats():
+    with open("stats", "r") as f:
+        return f.read().splitlines()
+
+def end_game(score):
+    with open("stats", "r+") as f:
+        lines = f.read().splitlines()
+        best = int(lines[0])
+
+        if score > best:
+            best = score
+
+        f.seek(0)
+        f.write(str(best))
+        f.write(f"\n{score}")
 
 print(f"Welcome to the PI Remember!")
 
 with (open("pi.txt", "r", encoding="UTF-8") as f):
     pi = f.read()
 
-    write_stats()
     for i in range(10000):
+        n = i + 1
+
         write_stats()
 
-        print(f"PI is: {pi[:i]}")
+        print(f"PI is: {pi[:n]}")
         time.sleep(1)
 
         os.system('cls' if os.name == 'nt' else 'clear')
@@ -23,9 +40,11 @@ with (open("pi.txt", "r", encoding="UTF-8") as f):
         write_stats()
         put = input("PI is? ")
 
-        if put != pi[:i]:
+        if put != pi[:n]:
             os.system('cls' if os.name == 'nt' else 'clear')
             print("Incorrect. GAME OVER!")
+            end_game(n)
+            time.sleep(5)
             break
 
         os.system('cls' if os.name == 'nt' else 'clear')
